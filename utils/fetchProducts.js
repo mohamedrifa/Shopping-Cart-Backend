2647459722f3ea42f2efe6df18c4e4d3eaec94f6
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-11-08 11:09:44 +0530

    final changejah

--- utils/fetchProducts.js
+++ utils/fetchProducts.js
@@ -1,13 +1,21 @@
 const axios = require("axios");
 
 exports.fetchFakeProducts = async () => {
-  const { data } = await axios.get("https://fakestoreapi.com/products?limit=100");
-  return data.map((p) => ({
-    id: p.id,
-    title: p.title,
-    price: p.price,
-    image: p.image,
-    category: p.category,
-    description: p.description,
-  }));
+  try {
+    const { data } = await axios.get(
+      "https://api.allorigins.win/raw?url=https://fakestoreapi.com/products?limit=100"
+    );
+
+    return data.map((p) => ({
+      id: p.id,
+      title: p.title,
+      price: p.price,
+      image: p.image,
+      category: p.category,
+      description: p.description,
+    }));
+  } catch (err) {
+    console.error("Failed to fetch products:", err.message);
+    throw new Error("API fetch failed");
+  }
 };
